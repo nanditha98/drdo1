bfa0526234cfaafd48b4e8ea0f8eed1674912722
nanditha98 <102796257+nanditha98@users.noreply.github.com>
2022-11-02 07:07:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+import Details from './components/Details';
+import Errror from './components/Error';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+function App() {
+  return (
+  <>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/details' element={<Details />} />
+      <Route path='*' element={<Errror />} />
+    </Routes>
+    </BrowserRouter>
+  </>
+  );
+}
+
+export default App;
